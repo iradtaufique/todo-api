bb26629639a7c24226a0a1be13686340f4328481
iradtaufique <iradukunda.ta@gmail.com>
2021-11-15 09:38:40 +0200

    add search by attributes, searchfields on todolist view

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework import permissions
+from rest_framework import permissions, filters
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from todo.models import Todo
 
 from todo.serializers import TodaSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 """ CreateAPIView is used to handle post request"""
@@ -34,6 +35,10 @@ class TodoListAPIView(ListAPIView):
 class CreateListTodos(ListCreateAPIView):
     serializer_class = TodaSerializer
     permission_class = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['id', 'title', 'desc', 'is_complete']
+    filterset_fields = ['id', 'title', 'desc', 'is_complete']
+    ordering_fields = ['id', 'title', 'desc', 'is_complete']
 
     """retrieve todos assigned to user"""
     def get_queryset(self):
