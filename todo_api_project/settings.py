5e279667c8059b52497e53ee071b71c664ac135f
iradtaufique <iradukunda.ta@gmail.com>
2021-11-16 13:23:59 +0200

    comment secret key

--- todo_api_project/settings.py
+++ todo_api_project/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRETE_KEY')
+# SECRET_KEY = os.environ.get('SECRETE_KEY')
+SECRET_KEY = 'django-insecure-%fw*+n!xjzwd))83%4^+3-q^x)a*&u)nm8!_e1g00shhi_6hf%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
