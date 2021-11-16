7e835d9b2ec86d275dc92b46a53c60b6f80d26df
iradtaufique <iradukunda.ta@gmail.com>
2021-11-16 12:58:25 +0200

    add whitenoize storage

--- todo_api_project/settings.py
+++ todo_api_project/settings.py
@@ -142,3 +142,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
